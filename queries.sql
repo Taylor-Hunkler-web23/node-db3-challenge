39f83fcfcecf5b9bc12dfa05b48809708ce47b66
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-06 16:24:46 -0500

    Wrote SQL for Display the OrderID, Customer's Company Name and the employee's LastName for every order

--- queries.sql
+++ queries.sql
@@ -10,3 +10,6 @@ select [Order].Id, [Order].ShipName, [Order].OrderDate from [Order] where [Order
 select Product.ProductName, OrderDetail.Quantity from OrderDetail
 join Product on OrderDetail.ProductId = Product.Id where OrderDetail.OrderId =10251 order by ProductName;
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select [Order].Id, [Customer].CompanyName, [Employee].LastName from [Order]
+join [Customer], [Employee] where [Order].CustomerId = [Customer].Id AND [Order].EmployeeId = [Employee].Id
