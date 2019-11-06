d973d489304993200f271f802dad485d1ec05afe
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-06 16:21:17 -0500

    Wrote SQL for Display the name and quantity of the products ordered in order with Id 10251

--- queries.sql
+++ queries.sql
@@ -7,4 +7,6 @@ join Category on Product.CategoryId = Category.Id;
 select [Order].Id, [Order].ShipName, [Order].OrderDate from [Order] where [Order].OrderDate<'2012-08-09'
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select Product.ProductName, OrderDetail.Quantity from OrderDetail
+join Product on OrderDetail.ProductId = Product.Id where OrderDetail.OrderId =10251 order by ProductName;
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
